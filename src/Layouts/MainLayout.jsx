73a96bfff835aa87c67fa6c8b205b2bac5cd2788
Redwan islam <redwanislam2760@gmail.com>
2024-03-03 23:00:48 +0600

    added custom scrollbar

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,26 +1,10 @@
-import { motion, useScroll } from "framer-motion";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/shared/Footer";
 import Navbar from "../components/shared/Navbar";
 
 const MainLayout = () => {
-  const { scrollYProgress } = useScroll();
-
   return (
     <div className="relative overflow-x-hidden">
-      <motion.div
-        style={{
-          scaleY: scrollYProgress,
-          width: "6px",
-          height: "100%",
-          position: "fixed",
-          transformOrigin: "top",
-          top: 0,
-          right: 0,
-          background: "#22C55E",
-          zIndex: 100,
-        }}
-      ></motion.div>
       <div className="w-full min-h-screen flex flex-col ">
         <Navbar />
         <div className="">
