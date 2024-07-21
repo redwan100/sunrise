35576755db28670dac231bed2053ef81ce3bbd1e
Redwan islam <ridoneislam987@gmail.com>
2024-07-21 15:41:37 +0600

    redesign mobile menubar

--- src/components/shared/navbar/MobileMenu.jsx
+++ src/components/shared/navbar/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BiChevronDown } from 'react-icons/bi';
+import { BiChevronDown, BiRightArrowAlt } from 'react-icons/bi';
 import { CgMenuRight } from 'react-icons/cg';
 import { IoIosCloseCircleOutline } from 'react-icons/io';
 import { Link } from 'react-router-dom';
@@ -64,30 +64,30 @@ const MobileMenu = () => {
         </div>
       </Container>
       <div
-        className={`bg-gray-800/80 backdrop-blur-lg text-white w-full min-h-screen fixed top-0 right-0 z-50 transition-all   ${showMenu ? 'translate-x-0' : 'translate-x-[120%]'}`}>
-        <div className="container mx-auto px-4 grid place-content-center min-h-screen space-y-4">
+        className={`bg-neutral-800 backdrop-blur-lg text-white w-full min-h-screen fixed top-0 right-0 z-50 transition-all duration-500  ${showMenu ? 'translate-x-0' : 'translate-x-[120%]'}`}>
+        <div className="container mx-auto px-4  min-h-screen space-y-4">
           <span
-            className="absolute top-3 right-5 text-white transition hover:text-red-500 text-2xl cursor-pointer"
+            className="absolute top-3 right-5 text-neutral-200 transition hover:text-red-500 text-2xl cursor-pointer"
             onClick={showMenuBar}>
-            <IoIosCloseCircleOutline />
+            <IoIosCloseCircleOutline className="" />
           </span>
           {navItems.map((item, index) => (
-            <div key={index}>
+            <div key={index} className="py-2">
               {item.submenu ? (
-                <div className="text-left">
+                <div className="text-left border-b border-b-neutral-600 w-full hover:border-b-neutral-400 transition-all">
                   <button
-                    className=" py-2 px-4 text-lg flex items-center uppercase hover:bg-slate-50 hover:text-gray-900 transition"
+                    className=" py-2 px-4 text-lg flex items-center justify-between w-full uppercase transition text-neutral-400 hover:text-neutral-100 "
                     onClick={() => handleMenuClick(index)}>
-                    {item.name} <BiChevronDown />
+                    {item.name} <BiChevronDown className="text-2xl" />
                   </button>
                   {openSubmenu === index && (
-                    <div className=" bg-slate-400 text-gray-800 ml-5 rounded-md">
+                    <div className=" bg-neutral-700 text-neutral-300 rounded-md">
                       {item.submenu.map((submenuItem, subIndex) => (
                         <Link
                           onClick={showMenuBar}
                           key={subIndex}
                           to={submenuItem.path}
-                          className="block py-2 px-4 text-sm hover:text-gray-50 font-semibold transition">
+                          className="block py-2 px-4 text-sm hover:text-gray-50 font-medium transition">
                           {submenuItem.name}
                         </Link>
                       ))}
@@ -97,14 +97,15 @@ const MobileMenu = () => {
               ) : (
                 <Link
                   to={item.path}
-                  className="block py-2 px-4 w-max text-lg uppercase hover:bg-slate-50 hover:text-gray-900 transition"
+                  className=" py-2 px-4 w-full text-lg uppercase text-neutral-400 hover:text-neutral-200 border-b border-b-neutral-600 hover:border-b-neutral-400 transition-all flex items-center justify-between group"
                   onClick={closeAllSubmenus}>
-                  {item.name}
+                  {item.name}{' '}
+                  <BiRightArrowAlt className="translate-x-9 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-300 text-3xl" />
                 </Link>
               )}
             </div>
           ))}
-          <button className="">
+          <button className="" onClick={closeAllSubmenus}>
             <Link to={'/donation-details'}>
               <DonationBtn text="donation" className={'py-[6px] text-base w-max ml-4'} />
             </Link>
